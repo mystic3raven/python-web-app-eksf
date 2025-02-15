389af95c082671aea526dece7f3c8173c4e099b7
mystic3eyedraven <mystic3eyedraven@gmail.com>
2025-02-14 17:36:47 -0800

    updated existing vpc and roles

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,10 +1,7 @@
 
 # Fetch existing VPC
 data "aws_vpc" "existing_vpc" {
-  filter {
-    name   = "tag:Name"
-    values = ["vpc-041a3b2e4a7f33dac"]  # Change to your VPC name
-  }
+  id = "vpc-041a3b2e4a7f33dac" # Use the actual VPC ID from AWS CLI
 }
 
 # Fetch existing public subnets
@@ -30,14 +27,14 @@ resource "aws_eks_cluster" "eks_cluster" {
   role_arn = data.aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids = data.aws_subnets.existing_public_subnets.ids  # Fetch existing subnets
+    subnet_ids = data.aws_subnets.existing_public_subnets.ids # Fetch existing subnets
   }
 }
 resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "eks-nodes-group"
-  node_role_arn   = data.aws_iam_role.eks_node_role.arn # Reference the existing IAM role
-  subnet_ids = data.aws_subnets.existing_public_subnets.ids  # Fetch existing subnets
+  node_role_arn   = data.aws_iam_role.eks_node_role.arn          # Reference the existing IAM role
+  subnet_ids      = data.aws_subnets.existing_public_subnets.ids # Fetch existing subnets
   scaling_config {
     desired_size = var.node_count
     min_size     = 1

549a1181ed199d2c85fd3449629f0536307a9fa2
mystic3eyedraven <mystic3eyedraven@gmail.com>
2025-02-19 03:22:44 -0800

    updated eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -8,4 +8,4 @@ module "eks" {
   source       = "./modules/eks"
   cluster_name = "python-web-app-eksf"
   subnet_ids   = module.vpc.private_subnet_ids
-}
+  role_arn     = module.iam.eks_role_arn  # 

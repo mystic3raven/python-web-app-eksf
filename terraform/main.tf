cc2119d5145c5a7ce13444d28e7b3bdc07ddc21d
mystic3eyedraven <mystic3eyedraven@gmail.com>
2025-02-19 02:46:52 -0800

    added fargate profile in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,16 @@ resource "aws_eks_cluster" "this" {
 
   depends_on = [aws_iam_role.eks_cluster]
 }
+
+resource "aws_eks_fargate_profile" "this" {
+  cluster_name           = aws_eks_cluster.this.name
+  fargate_profile_name   = "default"
+  pod_execution_role_arn = aws_iam_role.eks_fargate.arn
+  subnet_ids             = var.subnet_ids
+
+  selector {
+    namespace = "default"
+  }
+
+  depends_on = [aws_eks_cluster.this, aws_iam_role.eks_fargate]  # ✅ Ensure cluster is ready first
+}

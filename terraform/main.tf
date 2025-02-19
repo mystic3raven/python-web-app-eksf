319930066f533e62eb8417e364b0010b4e19928f
mystic3eyedraven <mystic3eyedraven@gmail.com>
2025-02-19 02:43:48 -0800

    main.tf added for kube cluster dependency

---
+++ terraform/main.tf
@@ -0,0 +1,10 @@
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
+
+  depends_on = [aws_iam_role.eks_cluster]
+}
